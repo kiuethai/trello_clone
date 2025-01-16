7a03417fd5aa03921285454bf0d2f3e9e9000ab1
Kieuthai1 <kieuthai093@gmail.com>
2025-01-17 00:44:57 +0700

    Drag Overlay & fix lỗi nhận diện quá của height và  thêm phần {listen} để chỉ phản ứng khi bấm đúng vào trong colummn

--- src/pages/Boards/BoardContent/BroadContent.jsx
+++ src/pages/Boards/BoardContent/BroadContent.jsx
@@ -5,6 +5,12 @@ import { mapOrder } from '~/utils/sorts'
 import { DndContext, PointerSensor, useSensor, useSensors, MouseSensor, TouchSensor } from '@dnd-kit/core'
 import { useEffect, useState } from 'react'
 import { arrayMove } from '@dnd-kit/sortable'
+
+const ACTIVE_DRAG_ITEM_TYPE = {
+  COLUMN: 'ACTIVE_DRAG_ITEM_TYPE_COLUMN',
+  CARD: 'ACTIVE_DRAG_ITEM_TYPE_CARD'
+}
+
 function BroadContent({ board }) {
   // fix trường hợp click bị gọi event
   // nếu dùng PointerSensor mặc định thì phải kết hợp thuộc tính CSS touch-action: none ở những phần trử kéo thả- nhưng mà còn bug 
@@ -18,12 +24,24 @@ function BroadContent({ board }) {
   const sensor = useSensors(mouseSensor, touchSensor)
 
   const [orderedColumns, setOrderedColumns] = useState([])
+
+  // cùng một thời điểm chỉ có một phần tiwr đang được kéo (column hoăc card)
+  const [ativeDragItemId, setAtiveDragItemId] = useState(null)
+  const [ativeDragItemType, setAtiveDragItemType] = useState(null)
+  const [ativeDragItemData, setAtiveDragItemData] = useState(null)
+
   useEffect(() => {
     setOrderedColumns(mapOrder(board?.columns, board?.columnOrderIds, '_id'))
   }, [board])
 
+  const handleDragStart = (event) => {
+    console.log('handleDragStart', event)
+    setAtiveDragItemId(event?.active?.id)
+    setAtiveDragItemType(event?.active?.data?.current?.columnId ? ACTIVE_DRAG_ITEM_TYPE.CARD : ACTIVE_DRAG_ITEM_TYPE.COLUMN)
+    setAtiveDragItemData(event?.active?.data?.current)
+  }
   const handleDragEnd = (event) => {
-    // console.log('handleDragEnd: ', event)
+    console.log('handleDragEnd: ', event)
     const { active, over } = event
     // kiểm tra nếu không tồn tại over
     if (!over) return
@@ -45,7 +63,11 @@ function BroadContent({ board }) {
     }
   }
   return (
-    <DndContext onDragEnd={handleDragEnd} sensors={sensor}>
+    <DndContext
+      sensors={sensor}
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+    >
       <Box
         sx={{
           bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1973d2'),
