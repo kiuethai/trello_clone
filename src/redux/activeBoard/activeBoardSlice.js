75854e8681d67f69c3596e63eb587dc38b4c4247
Kieuthai1 <kieuthai093@gmail.com>
2025-03-06 12:55:13 +0700

    cấu hình redux

---
+++ src/redux/activeBoard/activeBoardSlice.js
@@ -0,0 +1,78 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
+import { API_ROOT } from '~/utils/constants'
+import { mapOrder } from '~/utils/sorts'
+import { isEmpty } from 'lodash'
+import { generatePlaceholderCard } from '~/utils/formatters'
+
+
+
+// Khởi tạo giá trị State của một cái Sloce trong redux
+const initialState = {
+  currentActiveBoard: null
+}
+// Các hành động gọi api( bất đồng bộ) và cập nhật dữ liệu vào Redux, dùng Middleware createAsyncdThunk đi kèm với extraReducers
+export const fetchBoardDetailsAPI = createAsyncThunk(
+  'activeBoard/fetchBoardDetailsAPI',
+  async (boardId) => {
+    const response = await axios.get(`${API_ROOT}/v1/boards/${boardId}`)
+    // Lưu ý: axios sẽ trả kết quả về qua property của nó là data
+    return response.data
+  }
+)
+
+// Khởi tại một cái Slice trong kho lưu trữ - Redux Store
+export const activeBoardSlice = createSlice({
+  name: 'activeBoard',
+  initialState,
+  // Reducers: Nơi xử lý  dữ liệu đồng bộ
+  reducers: {
+    updateCurrentActiveBoard: (state, action) => {
+      const board = action.payload
+
+      // Xử lý dữ liệu  :v
+      // ...
+
+      // Update lại dữ liệu của cái currentActiveBoard
+      state.currentActiveBoard = board
+
+    }
+  },
+  // ExtraReducer: Nơi xử lý dữ liệu bất đồng bộ
+  extraReducers: (builder) => {
+    builder.addCase(fetchBoardDetailsAPI.fulfilled, (state, action) => {
+      // action.payload ở đây chính là cái responce.data trả về ở trên
+      let board = action.payload
+
+      // Sắp xếp thứ tự các column luôn ở đây trước khi đưa dữ liệu xuống bên dưới các component con
+      board.column = mapOrder(board?.columns, board?.columnOrderIds, '_id')
+      // Cần xử lý vấn đề kéo thả vào một column rỗng
+      board.columns.forEach(column => {
+        if (isEmpty(column.cards)) {
+          column.cards = [generatePlaceholderCard(column)]
+          column.cardOrderIds = [generatePlaceholderCard(column)._id]
+        } else {
+          // Sắp xếp thứ tự các column luôn ở đây trước khi đưa dữ liệu xuống bên dưới các component con
+          column.cards = mapOrder(column.cards, column.cardOrderIds, '_id')
+        }
+      })
+
+      // Update lại dữ liệu của cái currentActiveBoard
+      state.currentActiveBoard = board
+
+
+    })
+  }
+})
+
+// Action creators are generated for each case reducer function
+export const { updateCurrentActiveBoard } = activeBoardSlice.actions
+
+// Selectors:
+export const selecCurrentActiveBoard = (state) => {
+  return state.activeBoard.currentActiveBoard
+}
+
+
+// export default activeBoardSlice.reducer
+export const activeBoardReducer = activeBoardSlice.reducer
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,16 +1,14 @@
 let apiRoot = ''
 console.log('import.meta.env', import.meta.env)
 console.log('process.env', process.env)
-// eslint-disable-next-line no-constant-condition
-if (process.env.BUILD_MODE = 'dev') {
+// Môi trường Dev sẽ chạy localhost với port 8017
+if (process.env.BUILD_MODE === 'dev') {
   apiRoot = 'http://localhost:8017'
 }
-// eslint-disable-next-line no-constant-condition
-if (process.env.BUILD_MODE = 'production') {
+if (process.env.BUILD_MODE === 'production') {
   apiRoot = 'https://trello-api-1-3f5m.onrender.com'
-
 }
-console.log('🚀 ~ apiRoot:', apiRoot)
+// console.log('🚀 ~ file: constants.js:7 ~ apiRoot:', apiRoot)
 export const API_ROOT = apiRoot
 
 // export const API_ROOT = 'https://trello-api-1-3f5m.onrender.com'
